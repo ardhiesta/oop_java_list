bb600a0d241d2e33f75ffdb3174312bed5dc5373
ardhiesta <ardhi.mail@gmail.com>
2020-11-05 05:08:59 +0700

    add method changeListElement

--- src/oop/java/list/ListDemo2.java
+++ src/oop/java/list/ListDemo2.java
@@ -19,8 +19,20 @@ public class ListDemo2 {
         System.out.println(al);
 	}
 	
+	void changeListElement() {
+		// print ArrayList sebelum diubah
+		System.out.println("Initial ArrayList " + al);
+		
+		// ubah element index 2
+		al.set(2, "Universitas Sebelas Maret");
+		
+		// print ArrayList setelah diubah
+		System.out.println("Updated ArrayList " + al); 
+	}
+	
 	public static void main(String[] args) {
 		ListDemo2 ld2 = new ListDemo2();
 		ld2.addToList();
+		ld2.changeListElement();
 	}
 }
