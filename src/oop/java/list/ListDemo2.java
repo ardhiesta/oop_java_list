4f73bf582909cc9cac0d89960e499cac494bd995
ardhiesta <ardhi.mail@gmail.com>
2020-11-05 05:10:47 +0700

    add method iterateListELement

--- src/oop/java/list/ListDemo2.java
+++ src/oop/java/list/ListDemo2.java
@@ -5,34 +5,49 @@ import java.util.List;
 
 public class ListDemo2 {
 	// creating ArrayList
-	List<String> al = new ArrayList<>(); 
-	
+	List<String> al = new ArrayList<>();
+
 	// add ArrayList elements
 	void addToList() {
-		al.add("S1"); 
-        al.add("UNS"); 
-        
-        // menambahkan element di index 1
-        al.add(1, "Informatika");
-        
-        // print ArrayList
-        System.out.println(al);
+		al.add("S1");
+		al.add("UNS");
+
+		// menambahkan element di index 1
+		al.add(1, "Informatika");
+
+		// print ArrayList
+		System.out.println(al);
 	}
-	
+
 	void changeListElement() {
 		// print ArrayList sebelum diubah
 		System.out.println("Initial ArrayList " + al);
-		
+
 		// ubah element index 2
 		al.set(2, "Universitas Sebelas Maret");
-		
+
 		// print ArrayList setelah diubah
-		System.out.println("Updated ArrayList " + al); 
+		System.out.println("Updated ArrayList " + al);
 	}
-	
+
+	void iterateListELement() {
+		// Using the Get method and the
+		// for loop
+		for (int i = 0; i < al.size(); i++) {
+			System.out.print(al.get(i) + " ");
+		}
+
+		System.out.println();
+
+		// Using the for each loop
+		for (String str : al)
+			System.out.print(str + " ");
+	}
+
 	public static void main(String[] args) {
 		ListDemo2 ld2 = new ListDemo2();
 		ld2.addToList();
 		ld2.changeListElement();
+		ld2.iterateListELement();
 	}
 }
